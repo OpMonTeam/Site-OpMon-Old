ed22d82f541a2141a77f67c1435eb1c6dba63b8f
jlppc <jlppcprog@gmail.com>
2017-06-28 21:24:43 +0200

    Reorganization of all the website

--- js/custom.js
+++ js/custom.js
@@ -102,9 +102,9 @@ $(document).ready(function() {
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-       "images/home-bg-slideshow3.jpg",
+       "http://opmon-game.ga/images/home-bg-slideshow1.jpg", 
+       "http://opmon-game.ga/images/home-bg-slideshow2.jpg",
+       "http://opmon-game.ga/images/home-bg-slideshow3.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
