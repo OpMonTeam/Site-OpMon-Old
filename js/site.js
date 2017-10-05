c7f89963386e2cc82213dc1ff3481e6dceb0c8a8
Navet56 <evandib@gmail.com>
2017-10-05 18:23:35 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -4,7 +4,7 @@ function goTo(url){
 }
 
 var version = "0.13";
-var date = " (08/08/2017)";
+var date = " (05/10/2017)";
 
 function linuxDown(){
     goTo("https://github.com/jlppc/OpMon/releases/download/alpha-v" + version + "/OpMon_" + version  + "-bin.deb");
