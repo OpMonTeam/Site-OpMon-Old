aa525b9fe3aa8e577832566271b1569c582ed4c6
jlppc <jlppcprog@gmail.com>
2018-01-31 09:40:00 +0100

    Starting the new documentation : AGameScreen

--- pages/doc/files.js
+++ pages/doc/files.js
@@ -883,5 +883,43 @@ var opmon = [
     },
 ];
 
-var files = [main, initializer, opstring, stringkeys, internalfiles, optionssave, save, opteam, player, attack, attacks];
-var fnames = ["main.hpp", "Initializer.hpp", "OpString.hpp", "StringKeys.hpp", "InternalFiles.hpp", "OptionsSave.hpp", "Save.hpp", "Equipe.hpp", "Player.hpp", "Attack.hpp", "Attacks.hpp"];
+var AGameScreen = [{
+	name : "class AGameScreen",
+	shortName : "AGameScreen",
+	description : "Interface of all first-level controllers. <br/>A game screen is handled by the GameLoop.<br/>When running, two methods are called: processEvent() is called for each sf::Event detected, then update() is called once per frame.<br/>In addition, suspend() and resume() are called when respectively the controller loose the focus (another controller is added on top) and regain the focus.",
+	elements: [
+	{
+		name : "AGameScreen()",
+		shortName : "ctorAGameScreen",
+		description : "AGameScreen's constructor"
+	},
+	{
+		name : "virtual GameStatus checkEvent(sf::Event const&)",
+		shortName : "checkEvent",
+		description : "Process a new SFML input (keyboard, mouse, ...).",
+	},
+	{
+		name : "virtual GameStatus update() = 0",
+		shortName : "update",
+		description : "Update and draw the game. This method is called once per frame."
+	},
+	{
+		name : "virtual void suspend()",
+		shortName : "suspend",
+		description : "Suspend the game screen"
+	},
+	{
+		name : "virtual void resume()",
+		shortName : "resume",
+		description : "Resume the game screen"
+	},
+	{
+		name : "AGameScreen *getNextGameScreen()",
+		shortName : "getNextGameScreen",
+		description : "Returns the next game screen that must be printed after this one. When the method checkEvent will return GameStatus::NEXT, this method will be called to get the next game screen."
+	}
+	]
+}]
+
+var files = [AGameScreen];
+var fnames = ["Controller::AGameScreen"];
