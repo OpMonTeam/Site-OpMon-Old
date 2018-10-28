2e7c0b82d08e9e61c05c1acf98f25f07c8a2b5a3
Cyrion <jlppcprog@ntymail.com>
2018-10-28 16:24:20 +0100

    Improved a bit the documentation system + Redocumented AGameScreen, AnimationCtrl and EventsCtrl

--- pages/doc/doc.js
+++ pages/doc/doc.js
@@ -2,12 +2,15 @@ const TITLE       = document.getElementById("title");
 const LIST        = document.getElementById("list");
 const DESCRIPTION = document.getElementById("description");
 
-var docId = Number(location.search.substring(1).split("=")[1]);
+var docId = location.search.substring(1).split("=")[1];
+
+var depth = 0;
 
 function writeInList(tab){
+    depth++;
     var titleStr = "";
     for(var i = 0; i < tab.length; i++){
-	titleStr += ('<li><a href="#' + tab[i].shortName + '">' + tab[i].name + '</a>');
+	titleStr += ('<li><a href="#d' + depth + tab[i].shortName + '">' + tab[i].name[0] + '</a>');
 	if(tab[i].elements){
 	    titleStr += "<ul>";
 	    titleStr += writeInList(tab[i].elements);
@@ -15,44 +18,48 @@ function writeInList(tab){
 	}
 	titleStr += '</li>';
     }
+    depth--;
     return titleStr;
 }
 
+
+
 function writeInDesc(tab){
+    depth++;
     var descStr = "";
     for(var i = 0; i < tab.length; i++){
-	descStr += '<li><span class="likeH" id="' + tab[i].shortName + '"><code>' + tab[i].name + '</code></span><p>' + tab[i].description + '</p></li>';
+	descStr += '<li><span class="likeH" id="d' + depth + tab[i].shortName + '"><code>' + ((tab[i].name.length == 1) ? tab[i].name[0] : tab[i].name[1]) + '</code></span>' + (tab[i].inherited ? '<br/>Inherited from <code>' + tab[i].inherited + '</code><br/>' : "") + (tab[i].derived ? '<br/>Derived from <code>' + tab[i].derived + '</code><br/>' : "") + '<p>' + tab[i].description + '</p></li>';
 	if(tab[i].elements){
 	    descStr += "<ul>";
 	    descStr += writeInDesc(tab[i].elements);
 	    descStr += '</ul>'
 	}
     }
+    depth--;
     return descStr;
 }
 
 function printDoc(id){
-    if(id == NaN){
-	id = 0;
+    
+    if(files.get(id) != undefined){
+	var file = files.get(id)[0];
+	TITLE.innerHTML = files.get(id)[1];
+	var titleStr = "";
+	var descStr = "";
+	titleStr = "<code><ul>";
+
+	titleStr += writeInList(file);
+	
+	titleStr += "</ul></code>";
+	LIST.innerHTML = titleStr;
+	
+	descStr += writeInDesc(file);
+	
+	DESCRIPTION.innerHTML = descStr;
+    }else{
+	titleStr = "Unknown page ! Sorry.";
+	LIST.innerHTML = titleStr;
     }
-    var file = files[id];
-    
-    TITLE.innerHTML = fnames[id];
-    var titleStr = "";
-    var descStr = "";
-    titleStr = "<code><ul>";
-
-    titleStr += writeInList(file);
-
-    titleStr += "</ul></code>";
-    LIST.innerHTML = titleStr;
-
-    descStr += writeInDesc(file);
-    
-    DESCRIPTION.innerHTML = descStr;
 }
 
-if(docId == NaN){
-    docId = 0;
-}
 printDoc(docId);
