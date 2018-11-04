2e107ce60a63d83cd1848488f1e882a1ccca078d
Cyrion <jlppcprog@ntymail.com>
2018-11-04 10:42:49 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -3,14 +3,14 @@ function goTo(url){
     reload();
 }
 
-var version = "0.15";
-var date = " (13/08/2018)";
+var version = "0.15.1";
+var date = " (04/11/2018)";
 
 function linuxDown(){
     goTo("https://github.com/cyrioncentori/OpMon/releases/download/alpha-v" + version + "/OpMon-" + version  + "-Linux.deb");
 }
 function macDown(){
-	alert("Warning : 0.15 is not released on MacOS yet. You can download the 0.14 by clicking on \"Other versions\"\nAttention : La 0.15 n'est pas encore sortie sur MacOS. Vous pouvez télécharger la 0.14 en cliquant sur \"Autres versions\"");
+	alert("Warning : 0.15.1 is not released on MacOS yet. You can download the 0.14 by clicking on \"Other versions\"\nAttention : La 0.15 n'est pas encore sortie sur MacOS. Vous pouvez télécharger la 0.14 en cliquant sur \"Autres versions\"");
     //goTo("https://github.com/cyrioncentori/OpMon/releases/download/alpha-v" + version + "/OpMon-Mac_" + version + ".dmg");
 }
 function linuxDownScript(){
